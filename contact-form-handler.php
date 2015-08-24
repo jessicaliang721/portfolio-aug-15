28e9d2f82e0048ff80ca86d7ad08db99b70aac85
jessicapei <jessicapei93@gmail.com>
2015-08-24 10:39:41 -0500

    contact form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,78 +1,50 @@
 <?php
 
-	//only process POST requests
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		//get form fields and remove whitespace
-		$name = strip_tags(trim($_POST["name"]));
-			$name = str_replace(array("\r", "\n"), array(" "," "), $name);
-		$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-		$message = trim($_POST["message"]);
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
 
-		//check that data was sent to mailer, nothing blank
-		if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			//set 400 (bad request) and exit
-			http_response_code(400);
-			echo "There was a problem with your submission. Please complete the form and try again.";
-			exit;
-		}
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            exit;
+        }
 
-		//set recipient email address
-		$recipient = "jessicapei93@gmail.com";
+        // Set the recipient email address.
+        $recipient = "jessicapei93@gmail.com";
 
-		//set email subject
-		$subject = $_POST['subject'];
+        // Set the email subject.
+        $subject = trim($_POST['subject']);
 
-		//build email content
-		$email_content = "Name: $name\n";
-		$email_content .= "Email: $email\n\n";
-		$email_content .= "Message:\n$message\n";
+        // Build the email content.
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Message:\n$message\n";
 
-		//build email headers
-		$email_headers = "From: $name <$email>";
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
 
-		//send the email
-		if (mail($recipient, $subject, $email_content, $email_headers)) {
-			//set 200 (okay) response code
-			http_response_code(200);
-			echo "Thank you! Your message has been sent.";
-		}
-		else {
-			//set 500 (internal error) response code
-			http_response_code(500);
-			echo "Something went wrong and your message couldn't be sent.";
-		}
-	}
-	//not a POST request, set 403 (forbidden) reponse code
-	else {
-		http_response_code(403);
-		echo "There was a problem with your submission. Please try again.";
-	}
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- js/main.js
+++ js/main.js
@@ -4,33 +4,30 @@ $('nav').sticky();
 
 /* Back to top */
 
-jQuery(document).ready(function($){
-	// browser window scroll (in pixels) after which the "back to top" link is shown
-	var offset = 300,
-		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
-		//duration of the top scrolling animation (in ms)
-		scroll_top_duration = 700,
-		//grab the "back to top" link
-		$back_to_top = $('.cd-top');
+// browser window scroll (in pixels) after which the "back to top" link is shown
+var offset = 300,
+	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+	offset_opacity = 1200,
+	//duration of the top scrolling animation (in ms)
+	scroll_top_duration = 700,
+	//grab the "back to top" link
+	$back_to_top = $('.cd-top');
 
-	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) { 
-			$back_to_top.addClass('cd-fade-out');
-		}
-	});
+//hide or show the "back to top" link
+$(window).scroll(function(){
+	( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+	if( $(this).scrollTop() > offset_opacity ) { 
+		$back_to_top.addClass('cd-fade-out');
+	}
+});
 
-	//smooth scroll to top
-	$back_to_top.on('click', function(event){
-		event.preventDefault();
-		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
-	});
-
+//smooth scroll to top
+$back_to_top.on('click', function(event){
+	event.preventDefault();
+	$('body,html').animate({
+		scrollTop: 0 ,
+	 	}, scroll_top_duration
+	);
 });
 
 /* Contact form validation */
@@ -59,46 +56,51 @@ var validateForm = function() {
 /* Submit contact form using AJAX */
 
 $(function() {
-	//get the form
-	var form = $("#ajax-contact");
 
-	//get messages DIV
-	var formMessages = $("#form-messages");
+	// Get the form.
+	var form = $('#ajax-contact');
 
-	//event listener for contact form
-	$(form).submit(function(event) {
-		//stop browser from submitting form
-		event.preventDefault();
+	// Get the messages div.
+	var formMessages = $('#form-messages');
 
-		//serialize the form data
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
 		var formData = $(form).serialize();
 
-		//submit form using AJAX
+		// Submit the form using AJAX.
 		$.ajax({
 			type: 'POST',
 			url: $(form).attr('action'),
 			data: formData
-		}).done(function(response) {
-			//set message text
+		})
+		.done(function(response) {
+			// Set the message text.
 			$(formMessages).text(response);
 
-			//clear the form
-			$('#name').val(""); 
-			$('#email').val("");
-			$('#subject').val("");
-			$('#message').val("");
-		}).fail(function(data) {
-			//set the message text
-			if (data.responseText !== "") {
-				//use return text to set content for formMessages element
+			// Clear the form.
+			$('#name').val('');
+			$('#email').val('');
+			$('#message').val('');
+		})
+		.fail(function(data) {
+			// Set the message text.
+			if (data.responseText !== '') {
 				$(formMessages).text(data.responseText);
-			}
-			else {
-				$(formMessages).text('An error occured and your message could not be sent.');
+			} else {
+				console.log(data)
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
 			}
 		});
+
 	});
+
 });
+
+
 
 
 
